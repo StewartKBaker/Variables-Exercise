e136ef83b0e19117828160144b04808047227151
StewartBaker <bakrstewart16@gmail.com>
2024-07-09 09:57:14 -0500

    Changed code for project

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string myName = "Stewart";
+            int myAge = 20;
+            char lastInitial = 'B';
+            bool isLearning = true;
+            double confidence = 99.9;
+            decimal unHappy = 0.00m;
+                
+            
+            Console.WriteLine("Would you like to know about me?");
+            Console.WriteLine("\n(Y)es\n(N)o");
+            string interested = Console.ReadLine();
+
+            if (interested == "Y" || interested == "y")
+            {
+                Console.Clear();
+                Console.WriteLine($"Hello, my name is {myName} {lastInitial}!\nI am {myAge} years old and it is very {isLearning} that I am learning to code right now!\nI have about {confidence}% confidence in myself for this course and I am {unHappy}% unhappy");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Okay, Have a good day.");
+            }
+            
         }
     }
 }
